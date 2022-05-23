7a6d87e93d160ca5a7c12070919c29fd767c46a5
Eric Greene <eric@t4d.io>
2022-05-23 16:28:44 -0400

    delete color code demos

--- ToolsApp/ToolsApp.Web/Startup.cs
+++ ToolsApp/ToolsApp.Web/Startup.cs
@@ -35,7 +35,7 @@ namespace ToolsApp.Web
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
 
-            
+            services.AddSingleton<IColorsData, ColorsInMemoryData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
