513d9b55f7582dd11678e44c63e415eebe1915b8
Eric Greene <eric@t4d.io>
2022-05-25 13:41:43 -0400

    add unit test project

---
+++ ToolsAppComplete/ToolsApp.Components/Validators/HexColorAttribute.cs
@@ -0,0 +1,34 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ToolsApp.Components.Validators
+{
+  public class HexColorAttribute : ValidationAttribute
+  {
+    private Regex _hexColorRegex;
+
+    public HexColorAttribute(int hexColorLength)
+    {
+      _hexColorRegex = new Regex("^[0-9a-fA-F]{" + hexColorLength.ToString() + "}$");
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      var inputValue = Convert.ToString(value);
+
+      if (inputValue is null)
+      {
+        return new ValidationResult($"{validationContext.DisplayName} is not a valid hex color");
+      }
+
+      if (!_hexColorRegex.IsMatch(inputValue))
+      {
+        return new ValidationResult($"{validationContext.DisplayName} is not a valid hex color");
+      }
+
+      return ValidationResult.Success;
+    }
+  }
+
+}
---
+++ ToolsAppComplete/ToolsApp.Data/Models/Color.cs
@@ -0,0 +1,9 @@
+namespace ToolsApp.Data.Models
+{
+  public class Color
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Hexcode { get; set; }
+  }
+}
